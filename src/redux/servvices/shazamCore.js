f1224a528b161e96c50c3e62ec07504991a8adc8
tarekrashidy <tarekahmedelrashidy@gmail.com>
2023-05-14 15:53:29 +0300

    0

--- src/redux/servvices/shazamCore.js
+++ src/redux/servvices/shazamCore.js
@@ -1,4 +1,7 @@
-import {createApi,}
+    import {
+        createApi,
+        fetchBaseQuery
+    } from '@reduxjs/toolkit/query'
 const options = {
     method: 'GET',
     headers: {
@@ -11,4 +14,31 @@ fetch('https://shazam.p.rapidapi.com/charts/track', options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
-    export const shazamCoreApi= 
+    
+    export const shazamCoreApi = createApi({
+          reducerPath: 'shazamCoreApi',
+        // Set the baseUrl for every endpoint below
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://pokeapi.co/api/v2/'
+        }),
+        endpoints: (builder) => ({
+            getPokemonByName: builder.query({
+                // Will make a request like https://pokeapi.co/api/v2/pokemon/bulbasaur
+                query: (name) => `pokemon/${name}`,
+            }),
+            updatePokemon: builder.mutation({
+                query: ({
+                    name,
+                    patch
+                }) => ({
+                    url: `pokemon/${name}`,
+                    // When performing a mutation, you typically use a method of
+                    // PATCH/PUT/POST/DELETE for REST endpoints
+                    method: 'PATCH',
+                    // fetchBaseQuery automatically adds `content-type: application/json` to
+                    // the Headers and calls `JSON.stringify(patch)`
+                    body: patch,
+                }),
+            }),
+        }),
+    })
